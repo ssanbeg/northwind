36a1c9d2bb728ab73448384a7cfc9ce15cf40603
Daniel Schmitz <danieljfa@gmail.com>
2018-05-07 22:01:55 -0300

    a homepage

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('northwind.json')
 
+const options = {
+  static: 'public'
+}
+
+const middlewares = jsonServer.defaults(options)
 const port = process.env.PORT || 3000
 
-server.use(router)
+server.use(middlewares)
+server.use('/api', router)
 
 server.listen(port, () => {
   console.log('JSON Server is running on ' + port)
